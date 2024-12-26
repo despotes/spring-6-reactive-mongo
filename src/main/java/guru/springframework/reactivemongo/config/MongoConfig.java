9aa0091d58cd735738661ac03fac435e2b270cb9
Pedro Medeiros <pietromaddera@gmail.com>
2024-12-26 20:02:25 +0100

    305. Mongo Database Authentication

--- src/main/java/guru/springframework/reactivemongo/config/MongoConfig.java
+++ src/main/java/guru/springframework/reactivemongo/config/MongoConfig.java
@@ -1,11 +1,16 @@
 package guru.springframework.reactivemongo.config;
 
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -18,5 +23,15 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Override
     protected String getDatabaseName() {
         return "sfg";
+    }
+
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential
+                        .createCredential("root", "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                   settings.hosts((Collections.singletonList(new ServerAddress("localhost", 27017))));
+                });
     }
 }
