e197c41dc151fa6ff357923a2bcd1fb275ceeb7a
Pedro Medeiros <pietromaddera@gmail.com>
2024-12-28 17:19:40 +0100

    317. Webflux Handler

---
+++ src/main/java/guru/springframework/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,23 @@
+package guru.springframework.reactivemongo.web.fn;
+
+import guru.springframework.reactivemongo.domain.Beer;
+import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(beerService.getBeers(), BeerDTO.class);
+    }
+}
